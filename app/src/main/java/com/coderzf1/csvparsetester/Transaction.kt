70ed9cc7d0c17cbd52f6b43449955205d090f784
Carl Williams <42680657+TheRandomCrafter83@users.noreply.github.com>
2024-03-10 11:28:24 -0400

    Added some sample data to Transaction.kt

--- app/src/main/java/com/coderzf1/csvparsetester/Transaction.kt
+++ app/src/main/java/com/coderzf1/csvparsetester/Transaction.kt
@@ -13,3 +13,18 @@ data class Transaction(
 ){
     val uniqueKey:String = UUID.randomUUID().toString()
 }
+
+
+/* SAMPLE DATA: copy/paste this into a csv file
+
+"Account Name","Processed Date","Description","Check Number","Credit or Debit","Amount"
+"CHECKING",2024-02-29,"WITHDRAWAL CHECK CARD FIVE STAR FOOD",,"Debit",0.85
+"CHECKING",2024-02-29,"WITHDRAWAL CHECK CARD GOOGLE ",,"Debit",5.99
+"CHECKING",2024-02-29,"DEPOSIT PayCheck",,"Credit",663.87
+"CHECKING",2024-02-29,"WITHDRAWAL POS #1",,"Debit",4.70
+"CHECKING",2024-02-29,"WITHDRAWAL CHECK CARD GOOGLE",,"Debit",5.99
+"CHECKING",2024-02-07,"WITHDRAWAL POS #2",,"Debit",0.50
+"CHECKING",2024-02-06,"WITHDRAWAL POS #3",,"Debit",10.00
+"CHECKING",2024-02-06,"WITHDRAWAL POS #4",,"Debit",5.85
+
+*/
